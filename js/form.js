501abb6bd95fc8c0b9086999c9a4729abdd79e22
Javier Ruiz <jruiz@spartan-sys.com>
2016-07-04 08:44:12 +0200

    Primer commit, justo antes de crear la portada

---
+++ js/form.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Javier on 01/07/2016.
+ */
+var form = document.getElementById("form-contact");
+
+var otrosInput = document.getElementsByName("consulta");
+
+var inputOtros = document.createElement("input");
+inputOtros.setAttribute("id", "otros");
+inputOtros.setAttribute("type", "text");
+inputOtros.setAttribute("name", "otros");
+inputOtros.setAttribute("placeholder", "Como me has conocido");
+inputOtros.setAttribute("required", "");
+inputOtros.setAttribute("size", "80");
+
+
+for (var i = 0; i < otrosInput.length; i++) {
+    otrosInput[i].addEventListener('click', function(){
+        if (this.id == 'otro') {
+            this.parentNode.appendChild(inputOtros);
+        } else {
+            if(document.getElementById("otros")) {
+                this.parentNode.removeChild(inputOtros);
+            }
+        }
+    });
+}
+
---
+++ js/menu.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Javier on 02/07/2016.
+ */
+var navbarItems = document.getElementsByClassName('navbar-item');
+
+for (var i = 0; i < navbarItems.length; i++){
+    navbarItems[i].addEventListener('click', function(evt){
+
+        deleteActiveClass();
+
+        if (Modernizr.classList) {
+            this.classList.add('active');
+        } else {
+            this.classList += ' active';
+        }
+
+        var sectionToGo = this.getElementsByTagName('a')[0].href.split('#');
+
+        if(sectionToGo.length > 1) {
+            evt.preventDefault();
+            var goTo = sectionToGo[sectionToGo.length - 1];
+            getElementByIdAndScroll(goTo);
+        }
+    });
+}
+
+function deleteActiveClass() {
+    for(var i = 0; i < navbarItems.length; i++){
+        if (Modernizr.classList) {
+            navbarItems[i].classList.remove('active');
+            navbarItems[i].classList.remove('active-reverse');
+        } else {
+            navbarItems[i].className = 'navbar-item';
+        }
+    }
+}
+
+function getElementByIdAndScroll(name) {
+    var elem;
+    if (name == '') {
+        elem = document.getElementsByClassName('header')[0];
+    } else {
+        elem = document.getElementById(name);
+    }
+
+    scrollToElement(elem);
+}
+
+function scrollToElement(element) {
+    var jump = parseInt(element.getBoundingClientRect().top * .3);
+    document.body.scrollTop += jump;
+    document.documentElement.scrollTop += jump;
+
+    if (!element.lastJump || element.lastJump > Math.abs(jump)) {
+        element.lastJump = Math.abs(jump);
+
+        setTimeout(function() {
+            scrollToElement(element);
+        }, "60");
+
+    } else {
+        element.lastJump = null;
+    }
+}
